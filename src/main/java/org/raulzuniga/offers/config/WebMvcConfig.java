6ef08c68047df6ccae5fd07a3cbe15763db9385a
Raul Zuniga <rzuniga64@gmail.com>
2017-05-18 17:33:05 -0500

    Add notation @ComponentScan

--- src/main/java/org/raulzuniga/offers/config/WebMvcConfig.java
+++ src/main/java/org/raulzuniga/offers/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package org.raulzuniga.offers.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.view.JstlView;
  */
 @Configuration
 @EnableWebMvc
+@ComponentScan
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
     /**
