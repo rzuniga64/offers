bb9ea31b6185f85002b3a2d5d99080fe7dfadb37
Raul Zuniga <rzuniga64@gmail.com>
2017-05-18 22:38:00 -0500

    Update

--- src/main/java/org/raulzuniga/offers/config/WebMvcConfig.java
+++ src/main/java/org/raulzuniga/offers/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package org.raulzuniga.offers;
+package org.raulzuniga.offers.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
