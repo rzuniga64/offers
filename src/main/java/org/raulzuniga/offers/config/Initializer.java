52dfc94bb19fba0f6a104087baa4bea86282a41d
Raul Zuniga <rzuniga64@gmail.com>
2017-05-23 19:45:49 -0500

    Update

--- src/main/java/org/raulzuniga/offers/config/Initializer.java
+++ src/main/java/org/raulzuniga/offers/config/Initializer.java
@@ -3,6 +3,7 @@ package org.raulzuniga.offers.config;
 import org.raulzuniga.offers.WebMvcConfig;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
@@ -23,26 +24,30 @@ public class Initializer implements WebApplicationInitializer {
     public void onStartup(final ServletContext servletContext)
             throws ServletException {
 
-        /* Create the 'root' Spring Application context. */
-        AnnotationConfigWebApplicationContext mvcContext =
-                new AnnotationConfigWebApplicationContext();
-        mvcContext.register(WebMvcConfig.class);
-        mvcContext.setServletContext(servletContext);
-        mvcContext.refresh();
+        AnnotationConfigWebApplicationContext context = getContext();
+        context.setServletContext(servletContext);
+        context.refresh();
 
 		// Manage the lifecycle of the root application context
         // Spring needs to load our config file. -->
-		servletContext.addListener(new ContextLoaderListener(mvcContext));
-
-		// Create the dispatcher servlet's Spring application context
-		//AnnotationConfigWebApplicationContext dispatcherContext =
-				//new AnnotationConfigWebApplicationContext();
-		//dispatcherContext.register(DispatcherConfig.class);
+		servletContext.addListener(new ContextLoaderListener(context));
 
         /* Register and map the dispatcher servlet */
         ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher",
-                new DispatcherServlet(mvcContext));
+                new DispatcherServlet(context));
         dispatcher.setLoadOnStartup(1);
-        dispatcher.addMapping("/");
+        dispatcher.addMapping("/*");
+    }
+
+    /**
+     *  Create the 'root' Spring Application context.
+     *  @return the context
+     */
+    private AnnotationConfigWebApplicationContext getContext() {
+
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebMvcConfig.class);
+
+        return context;
     }
 }
