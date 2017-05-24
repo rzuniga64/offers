637b090d87b46a96138af9c3f384a9628bf84106
Raul Zuniga <rzuniga64@gmail.com>
2017-05-24 00:38:44 -0500

    Add Spring security configuration file.

---
+++ src/main/java/org/raulzuniga/offers/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package org.raulzuniga.offers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WeSecurityConfig class.
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/index").permitAll()
+                .antMatchers("/offers", "/newaccount").permitAll()
+                .antMatchers("/createaccount").permitAll()
+                .antMatchers("/accountcreated").permitAll()
+                .antMatchers("/denied").permitAll()
+                .antMatchers("/error").permitAll()
+                .antMatchers("/resource/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("rzuniga64").password("aggies92").roles("ROLE_ADMIN");
+    }
+}
