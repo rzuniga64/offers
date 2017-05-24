113aa9317705ebb8c350224f396888f231eb306f
Raul Zuniga <rzuniga64@gmail.com>
2017-05-24 00:41:32 -0500

    Add User service and data access object.

---
+++ src/main/java/org/raulzuniga/offers/dao/UsersDAO.java
@@ -0,0 +1,126 @@
+package org.raulzuniga.offers.dao;
+
+import org.raulzuniga.offers.models.User;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Users data access object.
+ */
+@Component("usersDao")
+public class UsersDAO {
+
+    /** JDBC template. */
+    private NamedParameterJdbcTemplate jdbc;
+    /** Password encoder. */
+    //@Resource
+    //private PasswordEncoder passwordEncoder;
+
+    /** Constructor. */
+    public UsersDAO() { }
+
+    /**
+     *  Get JDBC connection.
+     *  @return JDBC connection
+     */
+    public NamedParameterJdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    /**
+     *  Set JDBC connection.
+     *  Pass a DataSource instead of a JdbcTemplate to get access to some great
+     *  methods from DataSource which comes from org.apache.commons.dbcp.
+     *  BasicDataSource.
+     *  @param newJdbc newJdbc
+     */
+    @Resource
+    public void setNamedParameterJdbcTemplate(final DataSource newJdbc) {
+        this.jdbc = new NamedParameterJdbcTemplate(newJdbc);
+    }
+
+    /**
+     *  Create a User object in the database.
+     *  Create a set of parameters you can use to replace placeholders in your
+     *  SQL from your actual bean properties.
+     *
+     * @param user user
+     * @return true if an Offer object is returned; false otherwise
+     */
+    @Transactional
+    public boolean create(final User user) {
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+
+        params.addValue("username", user.getUsername());
+        // params.addValue("password", passwordEncoder.encode(user.getPassword()));
+        params.addValue("password", user.getPassword());
+        params.addValue("email", user.getEmail());
+        params.addValue("enabled", user.isEnabled());
+        params.addValue("authority", user.getAuthority());
+
+        jdbc.update("insert into springtutorial.users (username, password, email, enabled) "
+                + "values(:username, :password, :email, :enabled)", params);
+
+        return jdbc.update("insert into springtutorial.authorities (username, authority) "
+                + "values(:username, :authority)", params) == 1;
+    }
+
+    /**
+     * Username exists?
+     * @param username username
+     * @return true if exists; false otherwise
+     */
+    public boolean exists(final String username) {
+        return jdbc.queryForObject("select count(*) from springtutorial.users where binary username=:username",
+                new MapSqlParameterSource("username", username), Integer.class) > 0;
+
+    }
+
+    /**
+     * Take the SQL result and gives a RowMapper that maps each row to a User
+     * bean.
+     * @return all users
+     */
+    public List<User> getAllUsers() {
+
+        return jdbc.query("select * from springtutorial.users, "
+                        + "springtutorial.authorities "
+                        + "where binary users.username=authorities.username",
+                new BeanPropertyRowMapper<User>(User.class));
+    }
+
+    /**
+     *  Get all users. Alternative method to getAllUsers (not used).
+     *  @return offers
+     */
+    public List<User> getUsers() {
+
+        return jdbc.query("select * from springtutorial.users, "
+                + "springtutorial.authorities where binary users.username=authorities.username",
+                new RowMapper<User>() {
+
+            public User mapRow(final ResultSet resultSet, final int i)
+                    throws SQLException {
+
+                User user = new User();
+                user.setUsername(resultSet.getString("username"));
+                user.setEmail(resultSet.getString("email"));
+                user.setAuthority(resultSet.getString("authority"));
+                user.setEnabled(resultSet.getBoolean("enabled"));
+
+                return user;
+            }
+        });
+    }
+}
---
+++ src/main/java/org/raulzuniga/offers/service/UsersService.java
@@ -0,0 +1,63 @@
+package org.raulzuniga.offers.service;
+
+
+import org.raulzuniga.offers.dao.UsersDAO;
+import org.raulzuniga.offers.models.User;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ *  OffersService class. A class that provides the service of coordinating data
+ *  access objects.
+ *
+ *  @ Service annotation is equivalent to @Component.
+ *  We need an XML file to load our service, service-context.xml.
+ *  Autowiring our OffersDAO into OffersService. Then autowire OffersService
+ *  into OfferController in the OfferController class.
+ */
+@Service("usersService")
+public class UsersService {
+
+    /** Offers data access object. */
+    private UsersDAO usersDao;
+
+    /**
+     *  set the Users data access object.
+     *  @param newUsersDao offersDao
+     */
+    @Resource
+    public void setUsersDao(final UsersDAO newUsersDao) {
+        this.usersDao = newUsersDao;
+    }
+
+    /**
+     * Create a user.
+     * @param user user
+     */
+    public void create(final User user) {
+
+        usersDao.create(user);
+    }
+
+    /**
+     * Username exists?
+     * @param username username
+     * @return true if exists; false otherwise
+     */
+    public boolean exists(final String username) {
+        return usersDao.exists(username);
+    }
+
+    /**
+     * getAllUsers method.
+     * @return all users
+     */
+    @Secured("ROLE_ADMIN")
+    public List<User> getAllUsers() {
+
+        return usersDao.getAllUsers();
+    }
+}
