e57283945fe405b3a8816f633b7063062f3b7a12
Raul Zuniga <rzuniga64@gmail.com>
2017-05-30 22:18:37 -0500

    Add custom rowmapper.

---
+++ src/main/java/org/raulzuniga/offers/dao/OfferRowMapper.java
@@ -0,0 +1,29 @@
+package org.raulzuniga.offers.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.raulzuniga.offers.models.Offer;
+import org.raulzuniga.offers.models.User;
+import org.springframework.jdbc.core.RowMapper;
+
+public class OfferRowMapper implements RowMapper<Offer> {
+
+	@Override
+	public Offer mapRow(ResultSet rs, int rowNum) throws SQLException {
+		User user = new User();
+		user.setAuthority(rs.getString("authority"));
+		user.setEmail(rs.getString("email"));
+		user.setEnabled(true);
+		user.setName(rs.getString("name"));
+		user.setUsername(rs.getString("username"));
+
+		Offer offer = new Offer();
+		offer.setId(rs.getInt("id"));
+		offer.setText(rs.getString("text"));
+		offer.setUser(user);
+
+		return offer;
+	}
+
+}
